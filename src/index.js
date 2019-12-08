8aaa9a2d99f07cd321a64330ba7dc0467d29cf7f
LorenBaker <LorenABaker@comcast.net>
2019-12-07 19:38:24 -0800

    ReduxToastr Issue

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/layout/App';
 import { Provider } from 'react-redux';
+import ReduxToastr from 'react-redux-toastr';
+import 'react-redux-toastr/lib/css/react-redux-toastr.min.css';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { configureStore } from './app/store/configureStore';
@@ -15,22 +17,27 @@ store.dispatch(loadEvents());
 const rootEl = document.getElementById('root');
 
 let render = () => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <ScrollToTop>
-          <App />
-        </ScrollToTop>
-      </BrowserRouter>
-    </Provider>,
-    rootEl
-  );
+	ReactDOM.render(
+		<Provider store={store}>
+			<BrowserRouter>
+				<ScrollToTop>
+					<ReduxToastr
+						position='bottom-right'
+						transitionIn='fadeIn'
+						transitionOut='fadeOut'
+					/>
+					<App />
+				</ScrollToTop>
+			</BrowserRouter>
+		</Provider>,
+		rootEl
+	);
 };
 
 if (module.hot) {
-  module.hot.accept('./app/layout/App', () => {
-    setTimeout(render);
-  });
+	module.hot.accept('./app/layout/App', () => {
+		setTimeout(render);
+	});
 }
 
 render();
